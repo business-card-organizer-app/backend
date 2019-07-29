07436ac7c74c96e9f9c3723c97f5a5b5383ef481
Nmeregini Vincent <nmereginivincent@gmail.com>
2019-07-29 16:40:28 +0100

    -fixed heroku file path bug

--- knexfile.js
+++ knexfile.js
@@ -64,7 +64,10 @@ module.exports = {
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
     }
   }
 
